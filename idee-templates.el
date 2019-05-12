e8be25b900ab86bc1f0bdf1ddb6c66522f47ded5
Ioannis Canellos <iocanel@gmail.com>
2019-05-12 15:10:56 +0300

    feat: Project template suggestion is done using the template name instead of the file name.

--- idee-templates.el
+++ idee-templates.el
@@ -29,18 +29,26 @@
 (defconst idee-templates-dir
   (expand-file-name
    "templates"
-   (file-name-directory
-    ;; Copied from ‘yasnippet-snippets’ that copied from ‘f-this-file’ from f.el.
-    (cond
-     (load-in-progress load-file-name)
-     ((and (boundp 'byte-compile-current-file) byte-compile-current-file)
-      byte-compile-current-file)
-     (:else (buffer-file-name))))))
+   ;; If idee is installed using `straight-use-pacakge` we need to refer to the repo and not the build output.
+   ;; TODO: have these get downloaded from github.
+   (replace-regexp-in-string (regexp-quote "straight/build/idee") "straight/repos/idee"
+                             (file-name-directory
+                              ;; Copied from ‘yasnippet-snippets’ that copied from ‘f-this-file’ from f.el.
+                              (cond
+                               (load-in-progress load-file-name)
+                               ((and (boundp 'byte-compile-current-file) byte-compile-current-file)
+                                byte-compile-current-file)
+                               (:else (buffer-file-name))))))
 
 ;;
 ;; State
 ;;
 (defconst idee-emacs-templates-dir (concat (file-name-as-directory idee-resources-dir) "templates") "The directory where template files are stored.")
+
+(when (not (file-exists-p idee-emacs-templates-dir)) (mkdir idee-emacs-templates-dir))
+(add-to-list 'yas-snippet-dirs idee-emacs-templates-dir)
+(yas-compile-directory idee-emacs-templates-dir)
+(yas-reload-all)
 
 (defvar idee-type-modes-alist '() "Association list for extension to mode.")
 (setq idee-type-modes-alist '(
@@ -48,6 +56,7 @@
                                 ("org" . "org-mode")
                                 ("md" . "markdown-mode")
                                 ("java" . "java-mode")
+                                ("json" . "json-mode")
                                 ("js" . "js2-mode")
                                 ("ts" . "typescript-mode")
                                 ("py" . "python-mode")
@@ -56,6 +65,8 @@
                                 ("kt" . "kotlin-mode")
                                 ("groovy" . "groovy-mode")
                                 ("html" . "html-mode")
+                                ("yml" . "yaml-mode")
+                                ("yaml" . "yaml-mode")
                                 ))
 ;;
 ;; Functions
@@ -66,21 +77,44 @@
   (let* ((path (ido-find-file))
          (extension (file-name-extension (buffer-file-name path)))
          (mode (cdr (assoc extension idee-type-modes-alist)))
-         (filetypes (directory-files (concat (file-name-as-directory idee-emacs-templates-dir) mode)))
-         (filetype (projectile-completing-read "Select type of file:" filetypes)))
+         (mode-path (file-name-as-directory (concat (file-name-as-directory idee-emacs-templates-dir) mode)))
+         (filetypes (remove-if (lambda (f) (string-prefix-p "." f)) (directory-files mode-path)))
+         (definitions (mapcar (lambda (f) (idee-templates-parse-file (concat mode-path f))) filetypes))
+         (names (mapcar (lambda (d) (idee-snippet-name d)) definitions))
+         (name (projectile-completing-read "Select type:" names))
+         (matches (remove-if-not (lambda (d) (equal name (idee-snippet-name d))) definitions))
+         (key (car (mapcar 'idee-snippet-key matches))))
+
     (with-silent-modifications (write-file (buffer-file-name path)))
     (switch-to-buffer path)
-    (insert filetype)
+    (insert key)
     (funcall (intern mode))
     (yas-expand)))
 
 (defun idee-select-project-header-function ()
   "Select a header for the project from the existing selection of headers."
   (interactive)
-  (let ((headers (directory-files idee-emacs-headers-dir))
-        (header (projectile-completing-read "Select header:" headers))
-        (content (idee-read-and-eval-template (concat (file-name-as-directory idee-emacs-headers-dir) header))))
-        (idee--current-header content)))
+  (let* ((headers (directory-files idee-emacs-headers-dir))
+         (header (projectile-completing-read "Select header:" headers))
+         (content (idee-read-and-eval-template (concat (file-name-as-directory idee-emacs-headers-dir) header))))
+    (idee--current-header content)))
+
+
+; (KEY TEMPLATE NAME CONDITION GROUP VARS LOAD-FILE KEYBINDING UUID)
+
+(defun idee-templates-parse-file (f)
+  "Parse template from file F."
+  (with-temp-buffer
+    (insert-file-contents f)
+              (yas--parse-template f)))
+
+(defun idee-snippet-name (definition)
+  "Name of the snippet found in."
+  (car (cdr (cdr definition))))
+
+(defun idee-snippet-key (definition)
+  "Key of the snippet found in."
+  (car definition))
 
 (provide 'idee-templates)
 ;;; idee-templates.el ends here
