4094ec18864ee8d090d07d4ba87b45532f7256d5
Ioannis Canellos <iocanel@gmail.com>
2019-09-03 13:21:44 +0300

    feat: lsp-java-workspace-dir is now synced with treemacs workspace.

--- idee-java.el
+++ idee-java.el
@@ -51,6 +51,9 @@
 (defcustom idee-comment-java-custom-block-ending "**/\n" "Custom block comment ending to use when commenting java code." :group 'idee-java :type 'string)
 (defcustom idee-comment-java-custom-line-prefix " * " "Custom line prefix to use when commenting java code." :group 'idee-java :type 'string)
 
+
+(defcustom idee-java-treemacs-workspace-sync-enabled t "Use a workspace per treemacs workspace" :group 'idee-java :type 'boolean)
+
 (defconst java-comment-style (make-idee-comment-style :block-beginning "/*" :block-ending "*/"
                                                       :custom-block-beginning idee-comment-java-custom-block-beginning :custom-line-prefix idee-comment-java-custom-line-prefix :custom-block-ending idee-comment-java-custom-block-ending))
 (defvar idee--java-symbols)
@@ -206,6 +209,21 @@
      (lambda (x) (not (equal "" x))) ;;remove all empty strings (its usually the first)
      (split-string (replace-regexp-in-string "\\([A-Z]\\)" " \\1" s) " "))))
 
+(defun idee-java-switch-workspace (&optional w)
+  "Switch to workspace."
+  (let* ((current (treemacs-current-workspace))
+         (name (if current (treemacs-workspace->name current) nil))
+         (workspace
+          (cond
+           (w w)
+           ((and name idee-java-treemacs-workspace-sync-enabled (f-join (locate-user-emacs-file "workspace") name)))
+           (t (locate-user-emacs-file "workspace")))))
+
+    (message (format "Using LSP Java workspace: %s." workspace))
+    (setq lsp-java-workspace-dir workspace)
+    (setq lsp-java-workspace-cache-dir (f-join lsp-java-workspace-dir ".cache"))
+    (when (lsp-workspaces) (lsp-restart-workspace))))
+
 ;;; Visitor
 (defun idee-java-project-p (root)
   "Check if ROOT is the root path of a java project."
@@ -227,6 +245,8 @@
 (add-hook 'java-mode-hook 'idee-java-visit-file)
 
 (advice-add 'projectile-switch-to-buffer :after #'idee-java-visit-file)
+
+(add-hook 'treemacs-switch-workspace-hook 'idee-java-switch-workspace)
 
 (provide 'idee-java)
 ;;; idee-java.el ends here
